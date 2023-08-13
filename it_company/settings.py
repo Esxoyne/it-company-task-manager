c6726bf87581755bd2443cbffa72941fbe89adff
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-08-13 16:59:56 +0300

    Add AUTH_USER_MODEL to settings.py; set time zone

--- it_company/settings.py
+++ it_company/settings.py
@@ -100,13 +100,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "task_manager.Worker"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kyiv"
 
 USE_I18N = True
 
