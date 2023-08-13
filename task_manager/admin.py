a74b26694d1f8f1ed437fb245fe5d44b968cf97a
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-08-13 17:06:16 +0300

    Register Worker, Position, Task & TaskType in the admin; implement searching and filtering for Worker & Task

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from .models import Position, Task, TaskType, Worker
 
-# Register your models here.
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "position",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name", "description")
+    list_filter = ("is_completed", "priority", "task_type", "assignees")
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.unregister(Group)
