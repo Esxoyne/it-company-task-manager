fc6ae305d70d74424dfc59f30433c5590ccafe8d
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-08-28 23:24:40 +0300

    Create forms for Task & WorkerPositionUpdate

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import Worker
+from .models import Worker, Task
 
 
 class SignUpForm(UserCreationForm):
+
     class Meta(UserCreationForm.Meta):
         model = Worker
         fields = UserCreationForm.Meta.fields + (
@@ -14,3 +15,33 @@ class SignUpForm(UserCreationForm):
             "last_name",
             "email",
         )
+
+
+class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    deadline = forms.DateField(
+        widget=forms.DateInput(
+            attrs={"type": "date"}
+        )
+    )
+
+    class Meta:
+        model = Task
+        fields = (
+            "name",
+            "description",
+            "deadline",
+            "priority",
+            "task_type",
+            "assignees",
+        )
+
+
+class WorkerPositionUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Worker
+        fields = ("position",)
