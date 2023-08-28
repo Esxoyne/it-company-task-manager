fa4deec2581bdc296e04b86bd64408015f2c20a7
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-08-28 23:25:39 +0300

    Implement CRUD for Task, TaskType & Worker; configure URLconf

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,7 +6,66 @@ from . import views
 urlpatterns = [
     path("", views.Index.as_view(), name="home"),
     path("tasks/", views.TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", views.TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        views.TaskUpdateView.as_view(),
+        name="task-update",
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        views.TaskDeleteView.as_view(),
+        name="task-delete",
+    ),
+    path(
+        "tasks/<int:pk>/toggle-complete/",
+        views.toggle_task_complete,
+        name="toggle-task-complete",
+    ),
+    path(
+        "tasks/<int:pk>/toggle-assign/",
+        views.toggle_task_assign,
+        name="toggle-task-assign",
+    ),
+    path(
+        "task-types/",
+        views.TaskTypeListView.as_view(),
+        name="task-type-list",
+    ),
+    path(
+        "task-types/create/",
+        views.TaskTypeCreateView.as_view(),
+        name="task-type-create"
+    ),
+    path(
+        "task-types/<int:pk>/update/",
+        views.TaskTypeUpdateView.as_view(),
+        name="task-type-update"
+    ),
+    path(
+        "task-types/<int:pk>/delete/",
+        views.TaskTypeDeleteView.as_view(),
+        name="task-type-delete"
+    ),
     path("employees/", views.WorkerListView.as_view(), name="worker-list"),
+    path(
+        "employees/<int:pk>/",
+        views.WorkerDetailView.as_view(),
+        name="worker-detail",
+    ),
+    path(
+        "employees/<int:pk>/update/",
+        views.WorkerPositionUpdateView.as_view(),
+        name="worker-update",
+    ),
+    path(
+        "employees/<int:pk>/delete/",
+        views.WorkerDeleteView.as_view(),
+        name="worker-delete",
+    ),
     path("toggle-theme/", views.toggle_theme, name="toggle-theme"),
     path("sign-up/", views.SignUpView.as_view(), name="sign-up"),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from typing import Optional
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import SignUpForm
-from .models import Worker, Task
+from .forms import SignUpForm, TaskForm, WorkerPositionUpdateForm
+from .models import Worker, Task, TaskType
 
 
 class Index(LoginRequiredMixin, generic.ListView):
@@ -26,8 +28,115 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
 
 
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+
+
+class TaskDeleteView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    generic.DeleteView,
+):
+    model = Task
+    success_url = reverse_lazy("task_manager:task-list")
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+@login_required
+def toggle_task_complete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    task.is_completed = not task.is_completed
+    task.save()
+
+    return redirect(task.get_absolute_url())
+
+
+@login_required
+def toggle_task_assign(request, pk):
+    user = request.user
+    task = get_object_or_404(Task, pk=pk)
+
+    if task in user.tasks.all():
+        user.tasks.remove(pk)
+    else:
+        user.tasks.add(pk)
+
+    return redirect(task.get_absolute_url())
+
+
+class TaskTypeListView(LoginRequiredMixin, generic.ListView):
+    model = TaskType
+
+
+class TaskTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = TaskType
+    fields = ("name",)
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = TaskType
+    fields = ("name",)
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeDeleteView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    generic.DeleteView,
+):
+    model = TaskType
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+
 class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
+
+
+class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Worker
+
+
+class WorkerPositionUpdateView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    generic.UpdateView,
+):
+    model = Worker
+    form_class = WorkerPositionUpdateForm
+    success_url = reverse_lazy("task_manager:worker-list")
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+class WorkerDeleteView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    generic.DeleteView
+):
+    model = Worker
+    success_url = reverse_lazy("worker-list")
+
+    def test_func(self):
+        return self.request.user.is_staff
 
 
 class SignUpView(generic.CreateView):
