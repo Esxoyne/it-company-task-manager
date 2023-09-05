df3acdd2fdac36e4c4fac6d8f53b210ca89e9287
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-09-05 08:32:50 +0300

    Create SearchForms & TaskRenewForm; add deadline validator

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,6 +1,8 @@
+import datetime
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from .models import Worker, Task
 
@@ -18,9 +20,11 @@ class SignUpForm(UserCreationForm):
 
 
 class TaskForm(forms.ModelForm):
+
     assignees = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
         widget=forms.CheckboxSelectMultiple,
+        required=False,
     )
     deadline = forms.DateField(
         widget=forms.DateInput(
@@ -39,6 +43,32 @@ class TaskForm(forms.ModelForm):
             "assignees",
         )
 
+    def clean_deadline(self):
+        return validate_deadline(self.cleaned_data["deadline"])
+
+
+class TaskRenewForm(forms.ModelForm):
+
+    deadline = forms.DateField(
+        widget=forms.DateInput(
+            attrs={"type": "date"}
+        )
+    )
+
+    class Meta:
+        model = Task
+        fields = ("deadline",)
+
+    def clean_deadline(self):
+        return validate_deadline(self.cleaned_data["deadline"])
+
+
+def validate_deadline(deadline):
+    if deadline <= datetime.date.today():
+        raise ValidationError("Invalid date - deadline set in past")
+
+    return deadline
+
 
 class WorkerUpdateForm(forms.ModelForm):
 
@@ -49,3 +79,39 @@ class WorkerUpdateForm(forms.ModelForm):
             "last_name",
             "position",
         )
+
+
+class TaskSearchForm(forms.Form):
+
+    name = forms.CharField(
+        max_length=64,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by name"}
+        ),
+    )
+
+
+class WorkerSearchForm(forms.Form):
+
+    username = forms.CharField(
+        max_length=150,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by username..."}
+        ),
+    )
+
+
+class TaskTypeSearchForm(forms.Form):
+
+    name = forms.CharField(
+        max_length=32,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by name"}
+        ),
+    )
