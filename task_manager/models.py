520c001b18c6371708fb41b910779721dcff9e5c
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-09-08 01:37:41 +0300

    Create Project model; add type hints; minor model changes

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,13 +5,40 @@ from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    members = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="projects",
+    )
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_absolute_url(self) -> str:
+        return reverse("task_manager:project-detail", kwargs={"pk": self.pk})
+
+    @property
+    def progress(self) -> float:
+        total = self.tasks.count()
+        if not total:
+            return "0"
+
+        completed = self.tasks.filter(is_completed=True).count()
+        return completed / total
+
+    def get_progress(self) -> str:
+        return str(int(self.progress * 100))
+
+
 class Position(models.Model):
     name = models.CharField(max_length=32, unique=True)
 
     class Meta:
         ordering = ["name"]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -28,10 +55,10 @@ class Worker(AbstractUser):
         verbose_name_plural = "workers"
         ordering = ["id"]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.username} ({self.first_name} {self.last_name})"
 
-    def get_absolute_url(self):
+    def get_absolute_url(self) -> str:
         return reverse("task_manager:worker-detail", kwargs={"pk": self.pk})
 
 
@@ -41,7 +68,7 @@ class TaskType(models.Model):
     class Meta:
         ordering = ["name"]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -53,7 +80,12 @@ class Task(models.Model):
         URGENT = "urgent", "Urgent"
 
     name = models.CharField(max_length=64)
-    description = models.TextField()
+    description = models.TextField(blank=True)
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
     deadline = models.DateField()
     is_completed = models.BooleanField(default=False)
     priority = models.CharField(
@@ -74,18 +106,20 @@ class Task(models.Model):
     class Meta:
         ordering = ["deadline"]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
-    def get_absolute_url(self):
+    def get_absolute_url(self) -> str:
         return reverse("task_manager:task-detail", kwargs={"pk": self.pk})
 
-    def is_at_risk(self):
+    @property
+    def is_at_risk(self) -> bool:
         return (
             datetime.date.today()
             < self.deadline
             <= datetime.date.today() + datetime.timedelta(days=2)
         )
 
-    def is_overdue(self):
-        return self.deadline <= datetime.date.today()
+    @property
+    def is_overdue(self) -> bool:
+        return self.deadline <= datetime.date.today() and not self.is_completed
